015664e38c6746ef95b3bedba3b7edbaabe8d738
RIjvan-Juneja <junejarijvan8128@gmail.com>
2025-06-29 00:42:56 +0530

    add stc link on logo click

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -10,7 +10,7 @@ import log from '../lib/log.js';
 import MessageBoxType from '../lib/message-box.js';
 
 const onClickLogo = () => {
-    window.location = '/';
+    window.location = 'https://stc-creative-club.netlify.app/';
 };
 
 const onClickCheckUpdate = () => {
